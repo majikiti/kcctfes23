332671df91b856625eeebaa17a04b35a43eee1bc
YamaD <me@dyama.net>
2023-10-19 17:21:24 +0900

    add swup

--- astro.config.js
+++ astro.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config"
+import swup from "@swup/astro"
 
 export default defineConfig({
   site: "https://kcctfes23.kobe-kosen.org",
+  integrations: [swup({ theme: false })],
 })
