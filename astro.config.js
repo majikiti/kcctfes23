e602846b0d8e0f40743a711042181a24c7f6a5d1
YamaD <me@dyama.net>
2023-10-22 14:40:22 +0900

    a

--- astro.config.js
+++ astro.config.js
@@ -3,5 +3,6 @@ import swup from "@swup/astro"
 
 export default defineConfig({
   site: "https://kcctfes23.kobe-kosen.org",
+  scopedStyleStrategy : "class",
   integrations: [swup({ theme: false })],
 })
