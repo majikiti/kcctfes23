ddf0d58195fa26a1acf031dcda717bfd094f0874
YamaD <me@dyama.net>
2023-10-23 21:50:55 +0900

    wip

--- astro.config.js
+++ astro.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config"
+import svelte from "@astrojs/svelte"
 import swup from "@swup/astro"
 
 export default defineConfig({
   site: "https://kcctfes23.kobe-kosen.org",
-  scopedStyleStrategy : "class",
-  integrations: [swup({ theme: false })],
+  scopedStyleStrategy: "class",
+  integrations: [svelte(), swup({ theme: false })],
 })
---
+++ src/components/LinkCard.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  export let href: string
+  export let target: string | undefined
+  export let color: string
+  export let bgColor: string
+</script>
+
+<a {href} {target} style="--color:{color};--bgColor:{bgColor}">
+  <slot />
+</a>
+
+<style lang="scss">
+  a {
+    display: flex;
+    width: 16rem;
+    height: 10rem;
+    padding: 2px;
+
+    align-items: center;
+    justify-content: center;
+    border-radius: 1.2rem;
+
+    color: white;
+    background-color: var(--bgColor);
+    /* backdrop-filter: blur(8px); */
+    text-decoration: none;
+    font-weight: bold;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+</style>
